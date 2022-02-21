2daac7ea4e6ac9b6558479bb63b3ed17fa2b1d84
Hazel <xp0729@gmail.com>
2022-02-21 11:17:57 +0800

    add opensea v2 address

--- app/frontend/packs/opensea.js
+++ app/frontend/packs/opensea.js
@@ -2,7 +2,8 @@ const server = NODE_ENV["MORALIS_SERVER"];
 const appId = NODE_ENV["MORALIS_APPID"];
 Moralis.start({ serverUrl: server, appId: appId });
 
-const openseaAddress = NODE_ENV["OPENSEA_ADDRESS"];
+const openseaV1Address = NODE_ENV["OPENSEA_V1_ADDRESS"];
+const openseaV2Address = NODE_ENV["OPENSEA_V2_ADDRESS"];
 const loginAddress = localStorage.getItem("loginAddress");
 
 const checkOrder = async function() {
@@ -11,7 +12,7 @@ const checkOrder = async function() {
 
     if (txs.total > 0) {
         const result = $.grep(txs.result, function(tx) {
-            return tx.from_address == loginAddress && tx.to_address == openseaAddress
+            return tx.from_address == loginAddress && (tx.to_address == openseaV1Address || tx.to_address == openseaV2Address)
         })
 
         if (result.length > 0) {
